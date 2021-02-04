4d7dc2fefa1def6433e75a0dc3299d49bf887374
unknown <adriano.contacto@gmail.com>
2021-02-04 17:17:10 -0300

    Inicia proyecto
    Crea la estructura y pseudocodigo para luego abrir una branch

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+const state = document.querySelector('#state')
+const roundNumber = document.querySelector('#round-number')
+const square1 = document.querySelector('#cuadro1')
+const square2 = document.querySelector('#cuadro2')
+const square3 = document.querySelector('#cuadro3')
+const square4 = document.querySelector('#cuadro4')
+
+
+const machineMovements = []
+const userMovements = []
+
+
